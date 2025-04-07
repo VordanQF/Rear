42081d231b0be6b2c3c93584ed08a87207fdc058
vlkardakov <vlkardakov@gmail.com>
2025-04-08 00:23:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,23 @@
-import sqlite3
-import PyTelegramBotAPI
+from telebot import types
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from telebot.types import ReactionTypeEmoji
 
-connection = sqlite3.connect('db.sqlite3')
+from dotenv import load_dotenv
+import os, json, sqlite3, telebot
+
+conn = sqlite3.connect('db.sqlite3')
+curs = conn.cursor()
+
+API_TOKEN = os.getenv('TELEGRAM_BOT_API_TOKEN')
+
+def delete_message(message):
+    print(f'\n\n{message=}\n\n')
+    bot.delete_message(message['chat']['id'], message['message_id'])
 
 
+@bot.message_handler(commands=['start'])
+def cmd_start(message):
+    bot.send_message(message.chat.id, "Привет! Придумай тему для заказа: ")
+    bot.register_next_step_handler(message, process_task_type)
 
 connection.close()
